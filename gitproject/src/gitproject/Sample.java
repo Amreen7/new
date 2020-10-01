31b01b94c53d13d2d23443324e35ffac53b9d910
Aamreen <56421592+Amreen7@users.noreply.github.com>
2020-10-01 23:19:43 +0530

    exit added
    exit function

--- gitproject/src/gitproject/Sample.java
+++ gitproject/src/gitproject/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 	System.out.println("Intitiative by babbar and amreen");
 		System.out.println("Hello");//functions can be written
+		exit(0);//an exit function
 
 	}
 
