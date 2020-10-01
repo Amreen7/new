77b2a541ded264a7c4f7689baca2051ae8f2d919
Aamreen <56421592+Amreen7@users.noreply.github.com>
2020-10-01 23:21:38 +0530

    hack statement
    hacking statment

--- gitproject/src/gitproject/Sample.java
+++ gitproject/src/gitproject/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 	System.out.println("Intitiative by babbar and amreen");
 		System.out.println("Hello");//functions can be written
+		System.out.println("i love hacking");
 
 	}
 
